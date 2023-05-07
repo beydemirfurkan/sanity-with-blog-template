e729864fbc316d2600955185fcb63fe5851ca145
Furkan Beydemir <70379120+beydemirfurkan@users.noreply.github.com>
2023-05-08 00:12:42 +0300

    theme changed

--- app/(site)/blog/[post]/page.tsx
+++ app/(site)/blog/[post]/page.tsx
@@ -18,13 +18,13 @@ export default async function Post({ params } : Props) {
       <>
           <div className="mx-auto max-w-8xl">
               <div className="flex px-4 pt-8 lg:px-8">
-                  <Link className="flex text-sm font-semibold leading-6 group text-slate-700 hover:text-slate-900 dark:text-white dark:hover:text-emerald-300" href="/">
+                  <Link className="flex text-sm font-semibold leading-6 group text-slate-700 hover:text-slate-900 dark:text-white dark:hover:text-emerald-300" href="/blog">
                       <svg viewBox="0 -9 3 24" className="w-auto h-6 mr-3 overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-600">
                           <path d="M3 0L0 3L3 6" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /></svg>Go back</Link>
               </div>
           </div>
           <div className="max-w-4xl mx-auto my-24">
-              <dd className="inset-x-0 flex items-center gap-2 mb-3 text-slate-700 dark:text-gray-300"><BsCalendar2Date/> <time dateTime="2022-01-24T19:00:00.000Z"> Monday, January 24, 2022</time></dd>
+              <dd className="inset-x-0 flex items-center gap-2 mb-3 text-slate-700 dark:text-gray-300"><BsCalendar2Date/> <time>{new Date(post._createdAt).toLocaleDateString()}</time></dd>
               <h1 className="inline-block text-2xl font-extrabold tracking-tight sm:text-3xl text-slate-900 dark:text-slate-200">{post.name}</h1>
 
               {/* image goes here */}
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,52 +1,28 @@
-import { getPosts } from "@/sanity/sanity-utils";
+// import { getPosts } from "@/sanity/sanity-utils";
 import Link from "next/link";
-import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+// import { PortableText } from "@portabletext/react";
 
 export default async function Home() {
-  const posts = await getPosts();
+  // const posts = await getPosts();
 
   return (
     <>
-        <section>
-            {/* notice - end */}
-            <div className="flex flex-col items-center max-w-xl pt-8 pb-16 mx-auto text-center lg:pt-32 lg:pb-32">
-                <p className='my-6 flex gap-2.5 rounded-2xl border border-emerald-500/20 bg-emerald-50/50 p-4 leading-6 text-emerald-900 dark:border-emerald-500/30 dark:bg-emerald-500/5 dark:text-emerald-200 dark:[--tw-prose-links-hover:theme(colors.emerald.300)] dark:[--tw-prose-links:theme(colors.white)]'>@furkanbeydemir | frontend-developer</p>
-                <h1 className="mb-8 text-4xl font-bold text-black-800 sm:text-5xl md:mb-6 md:text-6xl dark:text-white">selam ben furkan!</h1>
-                <p className="mb-8 leading-relaxed text-gray-500 md:mb-12 xl:text-lg dark:text-gray-300">günlük hayatta karşılaştığım problemleri ve araştırmalarım sonucu edindiğim bilgileri bulabileceğiniz ufak çaplı bir blog :)</p>
-                <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
-                    <Link href="/blog" className="button-dark">Okumaya Başla</Link>
-                    <Link href="/contact" className="button-light">İletişim</Link>
-                </div>
-            </div>
-        </section>
+      <section>
+          <div className="flex flex-col items-center max-w-xl pt-8 pb-16 mx-auto text-center lg:pt-32 lg:pb-32">
+              <div>
+                  <img className="w-32 h-32 rounded-full mx-auto object-cover" height={32} width={32} src="/furkan_beydemir.jpg" alt="furkan beydemir" />
+              </div>
+              <p className='my-6 flex gap-2.5 rounded-lg border border-emerald-500/20 bg-emerald-50/50 px-4 py-2 leading-6 text-emerald-900 dark:border-emerald-500/30 dark:bg-emerald-500/5 dark:text-emerald-200'>frontend developer</p>
+              <h1 className="mb-8 text-4xl font-bold text-black-800 sm:text-5xl md:mb-6 md:text-6xl dark:text-white">selam ben furkan!</h1>
+              <p className="mb-8 leading-relaxed text-gray-500 md:mb-12  xl:text-lg dark:text-gray-300">günlük hayatta karşılaştığım problemleri ve araştırmalarım sonucu edindiğim bilgileri bulabileceğiniz ufak çaplı bir blog :)</p>
+              <div className="flex w-full flex-col gap-2.5 sm:flex-row sm:justify-center">
+                  <Link href="/blog" className="button-dark">Okumaya Başla</Link>
+                  <Link href="/contact" className="button-light">İletişim</Link>
+              </div>
+          </div>
+      </section>
         
-      <div className="grid grid-cols-1 gap-8 pb-24 md:grid-cols-2">
-        {posts.map((post) => (
-          <article
-            key={post._id}
-            className="post-item"
-          >
-            <h3 className="mt-0.5 text-lg font-bold text-gray-900 dark:text-gray-100">
-              {post.name}
-            </h3>
-            <div className="mt-2 text-gray-500 line-clamp-3 text-sm/relaxed">
-              <PortableText value={post.content} />
-            </div>
-            <Link
-              href={`/blog/${post.slug}`}
-              className="inline-flex items-center gap-1 mt-4 text-sm font-medium text-emerald-500 hover:text-emerald-600 dark:text-emerald-400"
-            >
-              Devamını gör
-              <span
-                aria-hidden="true"
-                className="block transition-all group-hover:ms-0.5 rtl:rotate-180"
-              >
-                →
-              </span>
-            </Link>
-          </article>
-        ))}
-      </div>
     </>
   );
 }
