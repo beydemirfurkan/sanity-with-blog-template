8eeaa41571c05239f236c9f863806ab4d723dd77
Furkan Beydemir <70379120+beydemirfurkan@users.noreply.github.com>
2023-05-14 12:25:46 +0300

    eslint edit

---
+++ app/(frontend)/blog/[post]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading () {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h2 className="font-extrabold text-3xl sm:text-6xl">Loading...</h2>
+        </div>
+    )
+}
--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,6 +2,7 @@ const config = {
     projectId: "sfzch8gd",
     dataset: "production",
     apiVersion: "2023-05-06",
+    useCdn: false,
 }
 
 export default config
